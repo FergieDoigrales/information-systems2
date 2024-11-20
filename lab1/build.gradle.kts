60489c29e1c26bdaca49879c6a4b5c94bcb8920d
fergie <doigralesFergie@gmail.com>
2024-11-20 08:32:26 +0300

    application fix

--- lab1/build.gradle.kts
+++ lab1/build.gradle.kts
@@ -23,26 +23,30 @@ repositories {
     mavenCentral()
 }
 
+
+
 dependencies {
-    implementation("org.eclipse.persistence:eclipselink:3.0.2")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa") {
-        exclude(group = "org.hibernate", module = "hibernate-core")
-    }
+    implementation("org.hibernate:hibernate-core:6.2.0.Final")
+    implementation("org.hibernate.validator:hibernate-validator:8.0.0.Final")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.postgresql:postgresql")
+    implementation("org.postgresql:postgresql:42.7.2")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation ("org.springframework.boot:spring-boot-starter-websocket")
     implementation ("org.modelmapper:modelmapper:3.2.1")
     implementation("com.auth0:java-jwt:4.4.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
